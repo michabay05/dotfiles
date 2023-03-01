e0f8b5df7008555c825ae3f78c76ee26afaf866c
michabay05 <111288777+michabay05@users.noreply.github.com>
2023-03-01 17:01:21 -0500

    Update neovide config

--- nvim_config/init.lua
+++ nvim_config/init.lua
@@ -1,14 +1,27 @@
 -- NEOVIDE CONFIG
 if vim.g.neovide then
-	vim.o.guifont = "Iosevka Nerd Font Mono:h12"
+	vim.o.guifont = "Iosevka Nerd Font Mono:h11"
 	vim.g.neovide_scale_factor = 1.0
 	vim.g.neovide_scroll_animation_length = 0.1
 	vim.g.neovide_refresh_rate = 59
 	vim.g.neovide_confirm_quit = true
 	vim.g.neovide_cursor_animation_length = 0
-end
---------------------------
 
+  vim.g.neovide_scale_factor = 1.0
+
+  local change_scale_factor = function(delta)
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+  end
+  vim.keymap.set("n", "<C-=>", function()
+    change_scale_factor(1.25)
+  end)
+  vim.keymap.set("n", "<C-->", function()
+    change_scale_factor(1/1.25)
+  end)
+
+end
+
+----------------------------------------
 require("user.packer")
 require("user.core.options")
 require("user.core.keymaps")
