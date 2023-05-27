47e5f1a7a2ea88f183e704a77a6c8a838ca6b16e
michabay05 <michabay05@gmail.com>
2023-05-27 19:00:02 -0400

    Another update to my configs

---
+++ emacs_config/elpa/evil-textobj-tree-sitter-20230521.414/evil-textobj-tree-sitter-pkg.el
@@ -0,0 +1,9 @@
+(define-package "evil-textobj-tree-sitter" "20230521.414" "Provides evil textobjects using tree-sitter"
+  '((emacs "25.1")
+    (tree-sitter "0.15.0"))
+  :commit "88f97e2defe57c1e2a8ff3bf9fb8d32c9bcdc6c1" :keywords
+  '("evil" "tree-sitter" "text-object" "convenience")
+  :url "https://github.com/meain/evil-textobj-tree-sitter")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- update.sh
+++ update.sh
@@ -1,14 +1,60 @@
-#! /bin/sh
+#!/usr/bin/bash
 
-set -xe
+update_vim_config() {
+    rm -rf ./vim_config/*
+    cp ~/.vimrc vim_config 
+    echo "Updated vim config..."
+}
 
 update_nvim_config() {
     rm -rf ./nvim_config
     cp -r ~/.config/nvim . 
     mv nvim nvim_config
-    git add .
-    git commit -m "Another update to my nvim config"
-    git push
+    echo "Updated nvim config..."
 }
 
-update_nvim_config
+update_i3_config() {
+    rm -rf ./i3_config
+    cp -r ~/.config/i3 . 
+    mv i3 i3_config
+    echo "Updated i3 config..."
+}
+
+update_emacs_config() {
+    rm -rf ./emacs_config
+    cp -r ~/.emacs.d . 
+    mv .emacs.d emacs_config
+    echo "Updated emacs config..."
+}
+
+all() {
+    update_vim_config
+    update_nvim_config
+    update_i3_config
+    update_emacs_config
+}
+
+if [[ -z "$1" ]]; then
+    echo "Please input type of argument: [ all, vim, nvim, i3, emacs ]"
+    exit 1
+fi
+
+if [[ "$1" == "all" ]]; then
+    all
+elif [[ "$1" == "vim" ]]; then
+    update_vim_config
+elif [[ "$1" == "nvim" ]]; then
+    update_nvim_config
+elif [[ "$1" == "i3" ]]; then
+    update_i3_config
+elif [[ "$1" == "emacs" ]]; then
+    update_emacs_config
+fi
+
+echo "Finished updating..."
+
+git add .
+git commit -m "Another update to my configs"
+git push
+
+echo "Committed and pushed changes."
