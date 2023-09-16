65691c21bb6fc791c1d7a2b3aafe1d6a4dd3e8ad
michabay05 <111288777+michabay05@users.noreply.github.com>
2023-09-16 12:05:58 -0400

    Delete emacs_config directory

--- emacs_config/elpa/ido-yes-or-no-20161108.2351/ido-yes-or-no-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from ido-yes-or-no-20161108.2351/ido-yes-or-no.el  -*- no-byte-compile: t -*-
-(define-package "ido-yes-or-no" "20161108.2351" "Use Ido to answer yes-or-no questions" '((ido-completing-read+ "0")) :commit "8953eadaaa7811ebc66d8a9eb7ac43f38913ab59" :authors '(("Ryan C. Thompson")) :maintainers '(("Ryan C. Thompson")) :maintainer '("Ryan C. Thompson") :keywords '("convenience" "completion" "ido") :url "https://github.com/DarwinAwardWinner/ido-yes-or-no")
