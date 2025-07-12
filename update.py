4cfc9ec8f4af80c0bef71b943e99353957a840d2
michabay05 <michabay05@gmail.com>
2025-07-12 00:05:46 -0400

    Added tmux config; removed old bash script
    the old bash script has officially been replaced by the newer python
    script

--- update.py
+++ update.py
@@ -1,17 +1,14 @@
 #! /usr/bin/python
 
 from pathlib import Path
-from datetime import datetime
-from typing import Iterator
-from filecmp import dircmp
-import os, shutil, sys
+import filecmp, os, shutil, sys
 
 
 def usage(program_name: str) -> None:
     print(f"Usage:")
     print(f"    {program_name} ACTION [OPTIONS]\n")
     print("ACTION:")
-    print("    detect - Provide a list of out-of-date configs")
+    print("    detect - Provide a list of out-of-date configs (system -> local)")
     print("    update - Sync out-of-date configs in system and in this directory")
     print("     setup - Copy configs from this directory to the system\n")
     print("OPTIONS:")
@@ -21,23 +18,59 @@ SYS_CONFIG_PATHS: list[tuple[str, str]] = [
     ("~/.config/alacritty", "alacritty"),
     ("~/.config/nvim", "nvim"),
     ("~/.config/zed", "zed"),
+    ("~/.vimrc", ".vimrc"),
+    ("~/.tmux.conf", ".tmux.conf"),
 ]
 
 def detect_out_of_date(act: bool) -> None:
-    curr_dir: Path = Path(__file__).parent
-    print(f"[INFO] Current working directory: '{curr_dir}'")
+    """Find a list of out of date config and maybe act upon them.
+
+        NOTE: The config found on the user's system is considered the source of truth. This
+              means that the files from the system are copied over into this directory.
+    """
+    diff_found: bool = False
+
     for sys_path, local_path in SYS_CONFIG_PATHS:
         if sys_path.startswith('~'):
             sys_path = f"{Path.home()}{sys_path[1:]}"
 
-        dcmp = dircmp(sys_path, local_path)
-        diffs: list[tuple[str, str]] = []
-        find_diff_files(dcmp, diffs, act=act)
-        for src, dst in diffs:
-            print(f"[INFO] Diff: {src} -> {dst}")
+        print(f"Checking '{sys_path}' vs '{local_path}'...")
+        sys_is_dir: bool = os.path.isdir(sys_path)
+        local_is_dir: bool = os.path.isdir(local_path)
+
+        if sys_is_dir ^ local_is_dir:
+            print(
+                f"[ERROR] Both sys_path and local_path should of the same type: either a file or directory"
+            )
+            sys.exit(1)
+
+        if sys_is_dir:
+            dcmp = filecmp.dircmp(sys_path, local_path)
+            diffs: list[tuple[str, str]] = []
+            if not diff_found and len(diffs) > 0:
+                diff_found = True
+
+            find_diff_files(dcmp, diffs, act=act)
+            for src, dst in diffs:
+                if act:
+                    print(f"[INFO]     Updated: {src} -> {dst}")
+                else:
+                    print(f"[INFO]     Diff: {src} -> {dst}")
+        else:
+            # If sys_path and local_path are not the same, then...
+            if not filecmp.cmp(sys_path, local_path):
+                diff_found = True
+                if act:
+                    shutil.copyfile(sys_path, local_path)
+                    print(f"[INFO]     Updated: {sys_path} -> {local_path}")
+                else:
+                    print(f"[INFO]     Diff: {sys_path} -> {local_path}")
+
+    if not diff_found:
+        print("[INFO] No difference found. Config found here is the same as the ones that exist on the system.")
 
 
-def find_diff_files(dcmp: dircmp, diffs: list[tuple[str, str]], act: bool) -> None:
+def find_diff_files(dcmp: filecmp.dircmp, diffs: list[tuple[str, str]], act: bool) -> None:
     for name in dcmp.diff_files:
         src_path = f"{dcmp.left}/{name}"
         dst_path = f"{dcmp.right}/{name}"
@@ -63,6 +96,8 @@ def find_diff_files(dcmp: dircmp, diffs: list[tuple[str, str]], act: bool) -> No
     for sub_dcmp in dcmp.subdirs.values():
         find_diff_files(sub_dcmp, diffs, act=act)
 
+
+
 if __name__ == "__main__":
     args = sys.argv
     program_name = args[0]
@@ -71,6 +106,10 @@ if __name__ == "__main__":
         print("[INFO] Please provide the correct parameters according to the usage above.")
         sys.exit(1)
 
+    curr_dir = os.getcwd()
+    print(f"[INFO] Current working directory: '{curr_dir}'")
+    print("================")
+
     action = args[1]
     match action:
         case "detect":
@@ -78,7 +117,7 @@ if __name__ == "__main__":
         case "update":
             detect_out_of_date(act=True)
         case "setup":
-            pass
+            raise NotImplementedError()
         case _:
             print(f"[ERROR] Unknown action type: '{action}'")
             print(f"[INFO] Refer the help list to find out the list of valid actions.")
